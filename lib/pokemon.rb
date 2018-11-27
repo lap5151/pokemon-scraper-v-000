02858c467929534d88c5cf6eb8c5f6cd6a3123e8
Lauren Pardee <lap5151@gmail.com>
2018-11-27 20:14:21 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,28 +1,49 @@
-require 'pry'
 class Pokemon
-  attr_accessor :id, :name, :type, :db, :hp
-
-  def initialize(id:, name:, type:, hp: nil,  db: )
-    @id = id
-    @name = name
-    @type = type
-    @hp = hp
-    @db = db
-  end
+  attr_accessor :id, :name, :type, :hp, :db
 
   def self.save(name, type, db)
-     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
-  def self.find(id,db)
-    new_pokemon = db.execute("SELECT id, name, type FROM pokemon WHERE id = ?", id).flatten
-    Pokemon.new(id: new_pokemon[0], name: new_pokemon[1], type: new_pokemon[2], db: new_pokemon[3])
+  def self.find(id_num, db)
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+  end
+
+  def initialize(id:, name:, type:, hp: nil, db:)
+    @id, @name, @type, @hp, @db = id, name, type, hp, db
   end
 
   def alter_hp(new_hp, db)
     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
-    #binding.pry
   end
-
-
 end
+
+# require 'pry'
+# class Pokemon
+#   attr_accessor :id, :name, :type, :db, :hp
+# 
+#   def initialize(id:, name:, type:, hp: nil,  db: )
+#     @id = id
+#     @name = name
+#     @type = type
+#     @hp = hp
+#     @db = db
+#   end
+# 
+#   def self.save(name, type, db)
+#      db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+#   end
+# 
+#   def self.find(id,db)
+#     new_pokemon = db.execute("SELECT id, name, type FROM pokemon WHERE id = ?", id).flatten
+#     Pokemon.new(id: new_pokemon[0], name: new_pokemon[1], type: new_pokemon[2], db: new_pokemon[3])
+#   end
+# 
+#   def alter_hp(new_hp, db)
+#     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+#     #binding.pry
+#   end
+# 
+# 
+# end
